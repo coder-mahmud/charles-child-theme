ef157a0c844adb09914cde5f36f20eadc4f92b40
Mahmud <mahmud.linked@gmail.com>
2018-10-29 05:57:26 +0600

    After zoom effect and bxslider fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function($){
 
-
     var containerEl = document.querySelector('.mix_container');
     var mixer = mixitup(containerEl);
 
@@ -8,16 +7,40 @@ jQuery(document).ready(function($){
     $('.see_more_part').click(function(){
     	//mySlider.reloadSlider(config);
 
-    	$(this).parent().find('.details_content').show('fast');
-    	$('.slider').bxSlider();
+    	$(this).parent().find('.details_content').addClass('show');
+    	//$('.slider').bxSlider();
     })
 	
 	$('.hide_this').click(function(){
-		$(this).parent().hide('slow');
+		$(this).parent().removeClass('show');
 	})
 
+	$('.slider').bxSlider();
 	
-//$('.slider').bxSlider();
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+
+
+
+
+
+
+
 	
 	console.log('working');
 
